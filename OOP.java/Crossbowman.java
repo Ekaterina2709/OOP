4ab21b1048788365fd2281da864d6f79288804c5
Екатерина Демина <111079583+Ekaterina2709@users.noreply.github.com>
2023-01-21 23:54:28 +0300

    дз 1

---
+++ OOP.java/Crossbowman.java
@@ -0,0 +1,9 @@
+
+public class Crossbowman extends Archer {
+   
+    public Crossbowman (String name){
+        super(name, 12, 10, new int[] {8,10}, 15, 9,16);
+    }            
+}
+
+
---
+++ OOP.java/Sniper.java
@@ -0,0 +1,8 @@
+
+public class Sniper extends Archer {
+    
+
+    public Sniper (String name){
+        super(name, 12, 10, new int[] {8,10}, 15, 9,32);
+    }            
+}
