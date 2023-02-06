95f85e59b0a1052477f23927d7ee0d02253f8e7b
Екатерина Демина <111079583+Ekaterina2709@users.noreply.github.com>
2023-02-07 01:40:56 +0300

    дз 2

--- ООП.java/Crossbowman.java
+++ ООП.java/Crossbowman.java
@@ -2,8 +2,7 @@
 public class Crossbowman extends Archer {
    
     public Crossbowman (String name){
-        super(name, 12, 10, new int[] {8,10}, 15, 9,16);
+        super(name,"Арбалетчик",12, 10, new int[] {8,10}, 15, 9,16);
     }            
 }
-
 
